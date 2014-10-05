30c82e47b012216bd072d45e88f81aaade6523e7
ay65535 <ay65535@icloud.com>
2014-10-05 17:26:24 +0900

    Initial commit

---
+++ app/src/main/java/com/github/ay65535/adttemplateproject/MainActivityTest.java
@@ -0,0 +1,21 @@
+package com.github.ay65535.adttemplateproject;
+
+import junit.framework.TestCase;
+
+public class MainActivityTest extends TestCase {
+
+	MainActivity activity;
+
+	public void setUp() throws Exception {
+		super.setUp();
+		activity = new MainActivity();
+	}
+
+	public void tearDown() throws Exception {
+
+	}
+
+	public void testOnCreate() throws Exception {
+		assertTrue(true);
+	}
+}
